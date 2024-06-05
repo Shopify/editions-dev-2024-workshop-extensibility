194c2d157e9d8423065c4c958050ba16dcb7549d
Steven Janczy <steven.janczy@shopify.com>
2024-06-05 09:16:41 -0400

    reverting to default cart transform

--- extensions/bundler-fn/src/run.ts
+++ extensions/bundler-fn/src/run.ts
@@ -1,73 +1,12 @@
 import type {
-  Input as GeneratedInput,
+  RunInput,
   FunctionRunResult,
-  MergeOperation,
-  CartTransform,
-  CartLine,
 } from "../generated/api";
 
-// TODO: make these hard-coded defaults configurable via metafield
-const DEFAULT_PERCENTAGE_DECREASE = 15;
-const DEFAULT_BUNDLE_SIZE = 2;
-
-const EMPTY_OPERATION = {
+const NO_CHANGES: FunctionRunResult = {
   operations: [],
 };
 
-interface Input extends GeneratedInput {
-  cartTransform: CartTransform & {
-    bundleParent: {
-      value: string;
-    };
-  };
-  cart: {
-    lines: (CartLine & {
-      canBundle: {
-        value: null | "true";
-      };
-    })[];
-  };
-}
-
-export function run(input: Input): FunctionRunResult {
-  const bundleParentId = input.cartTransform.bundleParent.value;
-  const cartHasBundle = input.cart.lines.some(
-    (line) =>
-      line.merchandise.__typename === "ProductVariant" &&
-      line.merchandise.product.id === bundleParentId,
-  );
-
-  if (!bundleParentId || input.cart.lines.length <= 1 || cartHasBundle) {
-    return EMPTY_OPERATION;
-  }
-
-  const bundleableCartlines = input.cart.lines.filter((line, idx) => {
-    if (line.canBundle?.value !== "true") return false;
-    if (idx >= DEFAULT_BUNDLE_SIZE) return false;
-
-    return true;
-  });
-
-  if (bundleableCartlines.length !== 2) {
-    return EMPTY_OPERATION;
-  }
-
-  return {
-    operations: [
-      {
-        merge: {
-          parentVariantId: bundleParentId,
-          price: {
-            percentageDecrease: {
-              value: DEFAULT_PERCENTAGE_DECREASE,
-            },
-          },
-          cartLines: bundleableCartlines.map((line) => ({
-            cartLineId: line.id,
-            quantity: 1,
-          })),
-        } as MergeOperation,
-      },
-    ],
-  };
-}
+export function run(input: RunInput): FunctionRunResult {
+  return NO_CHANGES;
+};
