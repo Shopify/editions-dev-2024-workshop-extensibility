42522b5ae0d28b14f25046a01ed88dc8c6f1e489
James Vidler <james.vidler@shopify.com>
2024-06-07 13:20:56 -0400

    keep separte files, undo changes to layout

---
+++ extensions/bundle-product-offer/src/BundleProductOffer.tsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import {
+  Button,
+  View,
+  Text,
+  Image,
+  InlineLayout,
+  InlineStack,
+  BlockLayout,
+  BlockSpacer,
+  useApplyCartLinesChange,
+  useApi,
+  SkeletonImage,
+  SkeletonText,
+} from "@shopify/ui-extensions-react/checkout";
+import type { CartLine, CartLineChange } from "@shopify/ui-extensions/checkout";
+import type { ProductVariant } from "@shopify/hydrogen/storefront-api-types";
+
+interface ExtensionProps {
+  recommendation?: {
+    productTitle: string;
+    productVariant: ProductVariant;
+  };
+  firstLine?: CartLine;
+}
+
+export function BundleProductOffer({ recommendation }: ExtensionProps) {
+  const [adding, setAdding] = useState(false);
+  const applyCartLinesChange = useApplyCartLinesChange();
+  const { i18n } = useApi();
+
+  if (!recommendation) {
+    return null;
+  }
+
+  const productPrice = i18n.formatCurrency(
+    Number(recommendation.productVariant.price.amount),
+  );
+
+  return (
+    <View
+      border="base"
+      background="subdued"
+      cornerRadius="base"
+      padding={["tight", "base", "base", "base"]}
+    >
+      <Text emphasis="bold">Add to your cart</Text>
+      <BlockSpacer />
+      <InlineLayout spacing="tight" columns={["fill", "20%"]}>
+        <InlineStack>
+          {recommendation.productVariant.image && (
+            <Image
+              cornerRadius="base"
+              accessibilityDescription={
+                recommendation.productVariant.image.altText || ""
+              }
+              source={recommendation.productVariant.image.url}
+            />
+          )}
+          <BlockLayout rows={["20%", 22]}>
+            <BlockSpacer />
+            <Text>
+              {recommendation.productVariant.title ||
+                recommendation.productTitle}
+            </Text>
+            <Text>{productPrice}</Text>
+          </BlockLayout>
+        </InlineStack>
+        <View maxBlockSize={10} minInlineSize="25%" inlineAlignment="end">
+          <Button loading={adding} disabled={adding} onPress={handleAddToCart}>
+            Add
+          </Button>
+        </View>
+      </InlineLayout>
+    </View>
+  );
+
+  async function handleAddToCart() {
+    if (!recommendation) {
+      return null;
+    }
+    const lineChange: CartLineChange = {
+      type: "addCartLine",
+      merchandiseId: recommendation.productVariant.id,
+      quantity: 1,
+    };
+
+    setAdding(true);
+    const result = await applyCartLinesChange(lineChange);
+    if (result.type === "error") {
+      console.error(result.message);
+    }
+    setAdding(false);
+  }
+}
+
+export function BundleProductOfferSkeleton() {
+  return (
+    <View
+      border="base"
+      background="subdued"
+      cornerRadius="base"
+      padding={["tight", "base", "base", "base"]}
+    >
+      <Text emphasis="bold">Add to your cart</Text>
+      <BlockSpacer />
+      <InlineLayout spacing="tight" columns={["fill", "20%"]}>
+        <InlineStack>
+          <SkeletonImage inlineSize={64} blockSize={64} />
+          <BlockLayout rows={["20%", 22]}>
+            <BlockSpacer />
+            <SkeletonText inlineSize="base" />
+            <SkeletonText />
+          </BlockLayout>
+        </InlineStack>
+        <View maxBlockSize={10} minInlineSize="25%" inlineAlignment="end">
+          <Button disabled={true}>Add</Button>
+        </View>
+      </InlineLayout>
+    </View>
+  );
+}
--- extensions/bundle-product-offer/src/index.tsx
+++ extensions/bundle-product-offer/src/index.tsx
@@ -2,25 +2,14 @@ import {
   reactExtension,
   useCartLines,
   useApi,
-  SkeletonText,
-  InlineLayout,
-  SkeletonImage,
-  BlockStack,
-  Divider,
-  Heading,
-  Button,
-  useApplyCartLinesChange,
-  Image,
-  Text,
 } from "@shopify/ui-extensions-react/checkout";
-import type {
-  CartLine,
-  Product,
-  ProductVariant,
-} from "@shopify/hydrogen/storefront-api-types";
+import type { Product } from "@shopify/hydrogen/storefront-api-types";
 
 import { useEffect, useState } from "react";
-import { CartLineChange } from "@shopify/ui-extensions/checkout";
+import {
+  BundleProductOffer,
+  BundleProductOfferSkeleton,
+} from "./BundleProductOffer";
 
 const productRecommendationsQuery = `#graphql
   query ProductRecommendations($productId: ID!) {
@@ -66,7 +55,6 @@ function Extension() {
   }, []);
 
   async function fetchProduct(productId?: string) {
-    setLoading(true);
     if (!productId) {
       return;
     }
@@ -108,117 +96,12 @@ function Extension() {
   }
 
   if (loading) {
-    return <LoadingSkeleton />;
-  } else if (product) {
+    return <BundleProductOfferSkeleton />;
+  }
+
+  if (product) {
     return <BundleProductOffer recommendation={product} />;
-  } else {
-    return null;
-  }
-}
-
-function LoadingSkeleton() {
-  return (
-    <BlockStack spacing="loose">
-      <Divider />
-      <Heading level={2}>You might also like</Heading>
-      <BlockStack spacing="loose">
-        <InlineLayout
-          spacing="base"
-          columns={[64, "fill", "auto"]}
-          blockAlignment="center"
-        >
-          <SkeletonImage aspectRatio={1} />
-          <BlockStack spacing="none">
-            <SkeletonText inlineSize="large" />
-            <SkeletonText inlineSize="small" />
-          </BlockStack>
-          <Button kind="secondary" disabled={true}>
-            Add
-          </Button>
-        </InlineLayout>
-      </BlockStack>
-    </BlockStack>
-  );
-}
-
-interface BundleProductOfferProps {
-  recommendation?: {
-    productTitle: string;
-    productVariant: ProductVariant;
-  };
-  firstLine?: CartLine;
-}
-
-function BundleProductOffer({ recommendation }: BundleProductOfferProps) {
-  const [adding, setAdding] = useState(false);
-  const applyCartLinesChange = useApplyCartLinesChange();
-  const { i18n } = useApi();
-
-  if (!recommendation) {
-    return null;
   }
 
-  const productPrice = i18n.formatCurrency(
-    Number(recommendation.productVariant.price.amount),
-  );
-
-  return (
-    <BlockStack spacing="loose">
-      <Divider />
-      <Heading level={2}>You might also like</Heading>
-      <BlockStack spacing="loose">
-        <InlineLayout
-          spacing="base"
-          columns={[64, "fill", "auto"]}
-          blockAlignment="center"
-        >
-          {recommendation.productVariant.image && (
-            <Image
-              border="base"
-              borderWidth="base"
-              borderRadius="loose"
-              source={recommendation.productVariant.image.url}
-              accessibilityDescription={recommendation.productTitle}
-              aspectRatio={1}
-            />
-          )}
-
-          <BlockStack spacing="none">
-            <Text>
-              {recommendation.productVariant.title ||
-                recommendation.productTitle}
-            </Text>
-            <Text appearance="subdued">{productPrice}</Text>
-          </BlockStack>
-          <Button
-            kind="secondary"
-            loading={adding}
-            accessibilityLabel={`Add ${recommendation.productTitle} to cart`}
-            disabled={adding}
-            onPress={handleAddToCart}
-          >
-            Add
-          </Button>
-        </InlineLayout>
-      </BlockStack>
-    </BlockStack>
-  );
-
-  async function handleAddToCart() {
-    if (!recommendation) {
-      return null;
-    }
-    const lineChange: CartLineChange = {
-      type: "addCartLine",
-      merchandiseId: recommendation.productVariant.id,
-      quantity: 1,
-    };
-
-    setAdding(true);
-    const result = await applyCartLinesChange(lineChange);
-    if (result.type === "error") {
-      console.error(result.message);
-    }
-    setAdding(false);
-  }
+  return null;
 }
